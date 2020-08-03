331be997c0d504a28a1986bc4574c5e9fcf9e0a1
cruisecg <cruisecg.cg@gmail.com>
2020-08-03 13:22:16 +0800

    modify - change tsconfig.json add type

---
+++ type/index.d.ts
@@ -0,0 +1,4 @@
+//放需要 reference 的檔案路徑
+///<reference path="../node_modules/phaser/types/SpineFile.d.ts" />
+/// <reference path="../node_modules/phaser/types/SpineGameObject.d.ts" />
+/// <reference path="../node_modules/phaser/types/SpinePlugin.d.ts" />
